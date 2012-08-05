a985d522c8de88428a7b1b18d6120f6b62d347b0
Prof. Bryant E. Adams <badams@wells.edu>
2012-08-05 13:53:22 -0400

    Adding template with sample headers

---
+++ cs131_program_X_template.c
@@ -0,0 +1,62 @@
+/**
+* @file   cs131_program_PROGRAMNUMBER_FLAST.c
+* @author YOUR_NAME_HERE
+* @date   DATE_HERE
+* @brief  Program N: QUICK_TITLE_FOR_PROGRAM
+*
+* @todo PUT_ASSIGNMENT_REQUIREMENTS_HERE
+* @done CHANGE_TODOs_TO_DONEs_WHEN_THEY_ARE_DONE
+* 
+* @status DESCRIBE_IF_EVERYTHING_WORKING_OR_WHAT_IS_NOT_YET_RIGHT
+* 
+* SHARING_YOUR_THOUGHT_PROCESS_HERE_MAY_HELP
+* WITH_GETTING_PARTIAL_CREDIT_IF_THINGS_ARE
+* NOT_ALL_CORRECT
+* 
+* @assistance CITE_NON_COURSE_WEBPAGES_YOU_USED_FOR_HELP
+* @assistance YOU_MAY_HAVE_MULTIPLE_CITATIONS
+* @assistance ALSO_CITE_PEOPLE_THAT_HELPED_YOU
+* @assistance I did not get help from source not listed above
+* @note STATE_WHO_YOU_HELPED,_IF_ANYONE
+* @note I did not give help to anyone not listed above
+*/
+
+#include <stdio.h>
+
+/**
+* @brief WHAT_IS_THIS_CONSTANT_FOR.
+*
+* ADD_MORE_DETAIL_ON_FOLLOWING_LINES_IF_APPROPRIATE 
+*/
+const int DAYS_IN_A_WEEK = 7;
+
+/**
+* @brief WHAT_THIS_FUNCTION_IS_FOR
+* @param PARAMNAME DESCRIPTION_OF_PARAMETER
+* @param ANOTHERPARAMNAME DESCRIPTION_OF_SECOND_PARAM
+* @sideeffect STATE_WHAT_IT_DOES
+* @sideeffect IT_MAY_DO_MULTIPLE_THINGS
+* @returns WHAT_IT_RETURNS
+*
+* MORE_DETAIL_GOES_HERE_IF_NEEDED
+*/
+int myfunc(int PARAMNAME, char ANOTHERPARAMNAME);
+
+/**
+* @brief Main procedure.
+*
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author YOUR_NAME_HERE
+*/
+int main( int argc, char * argv[] )
+{
+  //your program here
+  return 0;
+}
+
+int myfunc(int PARAMNAME, char ANOTHERPARAMNAME)
+{
+  //your function definition here
+}
