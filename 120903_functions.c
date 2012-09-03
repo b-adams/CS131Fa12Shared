f8f0d3f9d4079006aa2c47607245e5396858c0a2
Prof. Bryant E. Adams <badams@wells.edu>
2012-09-03 12:52:16 -0400

    Demonstrating use of values in addition to side effects

--- 120903_functions.c
+++ 120903_functions.c
@@ -3,34 +3,50 @@
 //Function to say "Happy Birthday Kailin" many times
 //No arguments - very static function
 //Declaration!
-char rejoice();
+void rejoice();
 
 //Partying will rejoice five times
-int party();
+void party();
+
+//Example with arguments and return values
+int addTwoNumbers(int firstNumber, int secondNumber);
 
 int main(int argc, char* argv[])
 {
   printf("v%d Hello, World!\n", 6);
   //Function call
-  party();
+  //party(); //too much partying, all done!
+  printf("Here's a number: %d\n", 4);
+  printf("Here's another number: %d\n", 3);
+  printf("Here's their total: %d\n", addTwoNumbers(3,4));
+  printf("Goodbye, %s World!\n", "cruel");
+  printf("%d letters", printf("YO!\n"));
   return 0;
 }
 
 //Definition!
-char rejoice()
+void rejoice()
 { //Body of definition
   printf("Happy Birthday Kailin!\n");
   printf("\tHappy Birthday Kailin!\n");
   printf("**\tHappy Birthday Kailin!\t**\n");
-  return 'q'; //Chosen arbitrarily
+  return ; //Chosen arbitrarily
 }
 
-int party()
+void party()
 {
   rejoice();
   rejoice();
   rejoice();
   rejoice();
   rejoice();
-  return 33;
+  printf("\n\n\t********\nThat's a lot of partying...\n\n\n");
+  party();
+  return;
+}
+
+int addTwoNumbers(int firstNumber, int secondNumber)
+{
+  printf("FYI, I am now adding %d and %d\n", firstNumber, secondNumber);
+  return firstNumber + secondNumber;
 }
