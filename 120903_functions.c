8f76db24de95e1e1b2ff593f6fd671c91c2b6b2a
Prof. Bryant E. Adams <badams@wells.edu>
2012-09-03 12:12:50 -0400

    First working version - mind the call

--- 120903_functions.c
+++ 120903_functions.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+//Function to say "Happy Birthday Kailin" many times
+//No arguments - very static function
+//Declaration!
+char rejoice();
+
 int main(int argc, char* argv[])
 {
-  printf("Hello, World!\n");
+  printf("v%d Hello, World!\n", 5);
+  //Function call
+  rejoice();
   return 0;
+}
+
+//Definition!
+char rejoice()
+{ //Body of definition
+  printf("Happy Birthday Kailin!\n");
+  printf("\tHappy Birthday Kailin!\n");
+  printf("**\tHappy Birthday Kailin!\t**\n");
+  return 'q'; //Chosen arbitrarily
 }
