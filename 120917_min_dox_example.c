02e3b3370002da41714a0b313c741662e09a367d
Prof. Bryant E. Adams <badams@wells.edu>
2012-09-17 11:49:31 -0400

    Adding mini documentation example

---
+++ 120917_min_dox_example.c
@@ -0,0 +1,32 @@
+/**
+ @file 120917_min_dox_example.c
+ @author Prof. Adams
+ @date Sept 17
+ @status Working
+*/
+
+//File documentation block comes
+//even before the includes, and
+//@file is the VERY first piece
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//Function declarations come next,
+//documentation goes right above
+//the associated declaration
+
+/**
+ @brief Main procedure.
+
+ @param argv the command line (typed arguments)
+ @param argc the number of options in the command line.
+ @return zero if successful
+ @author YOUR_NAME_HERE
+
+ This program... currently does nothing.
+*/
+int main(int argc, char* argv[])
+{
+ return 0;
+}
