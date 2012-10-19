ebc6651b8c3076f659a6add86680784eca565dc3
Prof. Bryant E. Adams <badams@wells.edu>
2012-10-19 11:30:52 -0400

    Adding factorial code

--- notes_by_date/121017_recursion.c
+++ notes_by_date/121017_recursion.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+/**
+ * @brief Find n!
+ * @param n Number to take factorial of
+ * @return 1*2*3*...*n
+*/
+F_fact(int n);
+
+/**
+ * @brief Find n!
+ * @param n Number to take factorial of
+ * @return 1*2*3*...*n
+*/
+R_fact(int n);
+
 int main(void)
 {
   return 0;
+}
+
+F_fact(int n)
+{
+  int runningProduct;
+  runningProduct=1;
+  for(int i=0; i<n; i++)
+  {
+    runningProduct=(i+1)*runningProduct;
+  }
+  return runningProduct;
+}
+
+R_fact(int n)
+{
+  int theProduct;
+  
+  if(n==0)
+  {
+    return 1; //Be done already, we know this one
+  } else if(n>0) {
+    theProduct = n*R_fact(n-1);
+  /*
+   Trusting documentation which indicates
+   R_fact(n-1) really is factorial of n-1
+  */
+  } else {
+    printf("WTF!?\n");
+  }
+  return theProduct;
 }
